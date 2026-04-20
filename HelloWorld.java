fba6f035521febb3265814ea79f534fde99b6535
debarghya2007 <debarghya2137@gmail.com>
2026-04-20 19:57:53 +0530

    UC3: Display Hello with argument or default 'World'

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        if (args.length > 0) {
-            String Debarghya = args[0];
-            System.out.println("Hello, " + Debarghya + "!");
-        } else {
-            System.out.println("Hello! Please provide your name as an argument.");
-        }
+        String name = (args.length > 0) ? args[0] : "World";
+        System.out.println("Hello, " + name + "!");
     }
 }
-
